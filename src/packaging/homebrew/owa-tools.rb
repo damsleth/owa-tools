44914cd58b7d97a01e4a1f1275c12349e8e492c0
Carl Joakim Damsleth <kim@damsleth.no>
2026-09-21 12:08:44 +0200

    packaging: mirror Homebrew formula for v1.6.0

--- src/packaging/homebrew/owa-tools.rb
+++ src/packaging/homebrew/owa-tools.rb
@@ -5,8 +5,8 @@ class OwaTools < Formula
 
   desc "Outlook/Microsoft 365 CLI suite (mail, calendar, graph, drive, todo, video)"
   homepage "https://github.com/damsleth/owa-tools"
-  url "https://github.com/damsleth/owa-tools/archive/refs/tags/v1.5.2.tar.gz"
-  sha256 "cd93a3f039b915288b21dfb015d975e3e18c241a975ba947698b451de8e6fe21"
+  url "https://github.com/damsleth/owa-tools/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "4af821c1940db2345101c8d575e36eb0a977cc099c6269dad40114281ed10151"
   license "MIT"
   head "https://github.com/damsleth/owa-tools.git", branch: "main"
 
